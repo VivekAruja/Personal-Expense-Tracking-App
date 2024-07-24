280dfa10df9760b549892e4e18ad630f8f47adcc
VivekAruja <62666867+VivekAruja@users.noreply.github.com>
2024-07-24 13:06:57 -0400

    Update expense.py

--- expense.py
+++ expense.py
@@ -1,8 +1,11 @@
 class Expense:
+    # Constructor to initialize an Expense object with name, category, and amount
     def __init__(self, name, category, amount) -> None:
-        self.name = name
-        self.category = category
-        self.amount = amount
+        self.name = name  # The name of the expense
+        self.category = category  # The category to which the expense belongs
+        self.amount = amount  # The amount of the expense
 
+    # Representation method to define how an Expense object is printed
     def __repr__(self):
         return f"<Expense: {self.name}, {self.category}, ${self.amount:.2f} >"
+        # Provides a string representation of the object, showing the name, category, and amount
